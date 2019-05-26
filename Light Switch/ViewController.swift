8e90dc2b50eca2a70ae44f80d89ed906dfb25930
Karina Macancela <karymacancela@gmail.com>
2019-05-26 11:36:14 -0400

    Added lights on and off functionalities

--- Light Switch/ViewController.swift
+++ Light Switch/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var isOn = true
     @IBOutlet weak var lightLabel: UILabel!
     
     override func viewDidLoad() {
@@ -17,9 +18,20 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    // when the button is pressed by user, the background color of the view will change to white, label text with change to "Lights On", and label color will change to black (to indicate that lights are on)
+    
     @IBAction func buttonWasPressed(_ sender: Any) {
-        lightLabel.text = "WOW"
-        lightLabel.textColor = .blue
+        if isOn {
+            view.backgroundColor = .white
+            lightLabel.text = "Lights On"
+            lightLabel.textColor = .black
+            isOn = false
+        } else { // same idea for turning lights off
+             view.backgroundColor = .black
+            lightLabel.text = "Lights Off"
+            lightLabel.textColor = .white
+            isOn = true
+        }
     }
     
 }
