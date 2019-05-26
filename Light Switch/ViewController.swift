081997b87c451c965bb4b07c06cb215d5d400f64
Karina Macancela <karymacancela@gmail.com>
2019-05-26 11:00:58 -0400

    Initial Commit

---
+++ Light Switch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Light Switch
+//
+//  Created by Karina Macancela on 5/26/19.
+//  Copyright © 2019 Karina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
