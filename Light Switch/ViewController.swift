fb0e693cc8d0078848284f8df1346f6ca00a5502
Karina Macancela <karymacancela@gmail.com>
2019-05-26 11:23:57 -0400

    Added IBOutlet and IBAction connections

--- Light Switch/ViewController.swift
+++ Light Switch/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonWasPressed(_ sender: Any) {
+        lightLabel.text = "WOW"
+        lightLabel.textColor = .blue
+    }
+    
 }
 
